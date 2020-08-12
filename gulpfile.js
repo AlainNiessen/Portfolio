585db4003f473e389b58b09c75e87b787f90eb5e
AlainNiessen <60482327+AlainNiessen@users.noreply.github.com>
2020-08-12 12:51:57 +0200

    Session 7
    Work on landing page:
    responsive work for tablette and PC
    
    Deleting all autoprefixes / browserslist options from gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require ('browser-sync').create();
 var minCss = require ('gulp-clean-css');
-var autoprefixer = require('gulp-autoprefixer');
 var rename = require('gulp-rename');
 
 //function that compile scss into css
@@ -15,25 +14,22 @@ function scss () {
     // 2. pass that file through the sass compiler
         .pipe(sass())
 
-    // 3.Autoprefixer after compiling
-        .pipe(autoprefixer())
-
-    // 4. in case of error, it will only show the exact error
+    // 3. in case of error, it will only show the exact error
         .on('error', sass.logError)    
 
-    // 6.where do I save the compiled css?
+    // 4.where do I save the compiled css?
         .pipe(gulp.dest('./css'))    
 
-    // 7. minimize css
+    // 5. minimize css
         .pipe(minCss())
 
-    // 8. rename
+    // 6. rename
         .pipe(rename({ prefix: 'min-' }))    
     
-    // 9.where do I save the minimalized css?
+    // 7.where do I save the minimalized css?
         .pipe(gulp.dest('./css'))
 
-    // 10. stream changes to all browsers / syncronize everything between different browsers / update css without refreshing browser
+    // 8. stream changes to all browsers / syncronize everything between different browsers / update css without refreshing browser
         .pipe(browserSync.stream());
 }
 
